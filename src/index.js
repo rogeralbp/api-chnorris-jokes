491e70f2844e1d23e81924f46b59b3f3c8dc9bcc
Roger Oporta <roportaperez@gmail.com>
2021-10-21 06:27:53 -0600

    Branch Renamed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "@babel/polyfill";
 
 
 import { init } from './js/users-page'
-import { getUsers } from './js/http-provider'
+
 
 init();
-//getUsers().then(console.log);
--- src/js/http-provider.js
+++ src/js/http-provider.js
@@ -2,30 +2,9 @@
 //Center Place to make requests
 
 
-const jokesURL = 'https://api.chucknorris.io/jokes/random';
+
 const usersURL = 'https://reqres.in/api/users?page=2';
 
-const getJoke = async() => {
-
-    try {
-
-        const response = await fetch( jokesURL );
-
-        if ( !response.ok ) throw 'Request can not be Completed!';
-
-        const { icon_url, id, value } = await response.json();
-
-        return { icon_url, id, value };
-
-
-    } catch (error) {
-
-        throw error;
-        
-    }
-   
-
-}
 
 const getUsers = async() => {
 
@@ -48,7 +27,6 @@ const getUsers = async() => {
 }
 
 export {
-
-    getJoke,
+   
     getUsers
 }
